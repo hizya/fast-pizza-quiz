f1c85e9135089cbbdd89a792d23783662a999e77
zhang-yuangg <2232945838@qq.com>
2023-12-15 15:45:23 +0800

    build-app-layout

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Home from './ui/Home';
+import Menu from './features/menu/Menu';
+import Order from './features/order/Order';
+import Cart from './features/cart/Cart';
+import CreateOrder from './features/order/Order';
+import AppLayout from './ui/AppLayout';
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/order',
+        element: <Order />,
+      },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+      },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Go.</Link>
+      <p>jonas</p>
+    </header>
+  );
+}
+
+export default Header;
