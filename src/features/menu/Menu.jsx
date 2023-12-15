54d8040be10f4e15152fcf119604ee354cf7caa2
zhang-yuangg <2232945838@qq.com>
2023-12-15 23:07:23 +0800

    fetch-data-react-router

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,24 @@
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+  return (
+    <ul>
+      {menu.map(pizza => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
 
+export async function loader() {
+  const menu = await getMenu();
+  // console.log(menu);
+  console.log('loader function');
+  return menu;
+}
+
+// loader();
 export default Menu;
