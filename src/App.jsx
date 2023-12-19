157b8927eb0a16625dbe2489f9ea87d48cdf90d8
zhang-yuangg <2232945838@qq.com>
2023-12-19 16:46:23 +0800

    fetching-order

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import Home from './ui/Home';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Order, { loader as searchOrder } from './features/order/Order';
 import Cart from './features/cart/Cart';
-import CreateOrder from './features/order/Order';
+import CreateOrder, {
+  action as formAction,
+} from './features/order/CreateOrder';
 import AppLayout from './ui/AppLayout';
 import Error from './ui/Error';
 const router = createBrowserRouter([
@@ -29,6 +31,7 @@ const router = createBrowserRouter([
       {
         path: '/order/new',
         element: <CreateOrder />,
+        action: formAction,
       },
       {
         path: '/order/:orderId',
